fb4ffe28a17109d55d04817b172d246820377a45
David Clarke <drclarke99@gmail.com>
2023-05-31 19:21:19 -0700

    Update Row.tsx

--- components/Row.tsx
+++ components/Row.tsx
@@ -38,7 +38,7 @@ function Row({title, movies}: Props) {
             {title}
         </h2>
         <div className='group relative md:-ml-2'>
-            <ChevronLeftIcon className={`absolute top-o bottom-0 left-2 z-40 m-auto h-9 w-9 
+            <ChevronLeftIcon className={`absolute top-0 bottom-0 left-2 z-40 m-auto h-9 w-9 
             cursor-pointer transition hover:scale-125 group-hover:opacity-100 opacity-0 
             ${!isMoved && 'hidden'}
             `}
@@ -51,7 +51,7 @@ function Row({title, movies}: Props) {
                 ))}
             </div>
 
-            <ChevronRightIcon className='absolute top-o bottom-0 right-2 z-40 m-auto h-9 w-9 
+            <ChevronRightIcon className='absolute top-0 bottom-0 right-2 z-40 m-auto h-9 w-9 
             cursor-pointer transition hover:scale-125 group-hover:opacity-100 opacity-0'
             onClick={() => handleClick("right")}/>
         </div>
